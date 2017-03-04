f49c4ee27de4a5d956ef26638e23fdc78cc69269
Krisofer Brink <kpgbrink@gmail.com>
2017-03-04 11:39:41 -0500

    make user different way

--- project/create.sql
+++ project/create.sql
@@ -1,9 +1,33 @@
 
 
-GRANT CONNECT TO schedule_helper IDENTIFIED BY password;
-GRANT UNLIMITED TABLESPACE TO schedule_helper;
+--GRANT CONNECT TO schedule_helper IDENTIFIED BY password;
+--GRANT UNLIMITED TABLESPACE TO schedule_helper;
 
 
 
+--@load.sql
+
+-- Create the Schedule helper church user and database. 
+-- See ../README.txt for details.
+
+-- Create the user.
+DROP USER kpb23 CASCADE;
+CREATE USER kpb23 
+	IDENTIFIED BY bjarne;
+GRANT 
+	CONNECT,
+	CREATE TABLE,
+	CREATE SESSION,
+	CREATE SEQUENCE,
+	CREATE VIEW,
+	CREATE MATERIALIZED VIEW,
+	CREATE PROCEDURE,
+	CREATE TRIGGER,
+	UNLIMITED TABLESPACE
+	TO kpb23;
+
+-- Connect to the user's account/schema.
+CONNECT kpb23/bjarne;
+
+-- Load database
 @load.sql
-
