fa6d13eabfa2e8b7d16bb6766882cf042dcf5184
Kristofer <kpgbrink@gmail.com>
2017-03-15 11:04:32 -0400

    guide07

--- 07sql/guide07.sql
+++ 07sql/guide07.sql
@@ -25,3 +25,17 @@
 -- Materialized view is a replica of a target master from a point in time.
 -- this helps ease network loads.
 -- Query modification. Instant.
+
+-- Formal languages for the relational model (Chapter 8, for Wednesday)
+
+-- a. Relational Algebra (read Sections 8.1–8.3 & 8.5) — Write a simple query on the movies database using SELECT (scondition), PROJECT (pfieldlist), RENAME (?newName) and JOIN (?condition) (see example queries 1 & 2 in Section 8.5).
+SELECT (Person), PROJECT (Person), RENAME (Person2) and JOIN (Person2.lastName = Person.lastname)
+-- b. Tuple Relational Calculus (read Sections 8.6.1–8.6.4 & 8.6.8) — Write a simple query on the movies database using the tuple relational calculus queries (see example queries 0 & 1 in Section 8.6.4).
+{ p.firstName, p.lastName | Person(p) AND (Existential d)
+(HomeGroup(hg)AND hg.name = 'Home1' AND p.homeGroupId = hg.id)}
+
+-- c.Define the following terms:
+--    i. Existential (?) and universal (?) quantifiers (see Section 8.6.3).
+--       Existential: there exists.. ,  Universal: all that exist..
+--    ii. Safe expressions (see Section 8.6.8).
+-- guaranteed to yield a finite number of tuples as its result; otherwise unsafe.
