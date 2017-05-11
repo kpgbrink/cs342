ec58aa15fe782516a3566f1f71ba1126ab11d2fd
Kristofer Brink <kpb23@calvin.edu>
2017-05-11 16:08:44 -0400

    homework13 process

--- 13postrelational/Homework13/src/LoadDB.java
+++ 13postrelational/Homework13/src/LoadDB.java
@@ -15,23 +15,46 @@ public class LoadDB {
                 "jdbc:oracle:thin:@localhost:1521:xe", "imdb", "bjarne");
         Statement jdbcStatement = jdbcConnection.createStatement();
 
-        ResultSet resultSet = jdbcStatement.executeQuery("SELECT id, name, year, rank FROM Movie");
+        ResultSet movieResult = jdbcStatement.executeQuery("SELECT id, name, year, rank FROM Movie");
 
         // Load the Movies for Get Table Values from the movie table
-        while (resultSet.next()) {
-            System.out.println(resultSet.getInt(1) + "\t" + resultSet.getString(2) + "\t"
-                    + resultSet.getString(3) + "\t" + resultSet.getString(4));
+        while (movieResult.next()) {
+            System.out.println(movieResult.getInt(1) + "\t" + movieResult.getString(2) + "\t"
+                    + movieResult.getString(3) + "\t" + movieResult.getString(4));
 
-            String movieIdString = resultSet.getString(1);
+            String movieIdString = movieResult.getString(1);
             Key name = Key.createKey(Arrays.asList("movie", movieIdString), Arrays.asList("name"));
             Key year = Key.createKey(Arrays.asList("movie", movieIdString), Arrays.asList("year"));
             Key rating = Key.createKey(Arrays.asList("movie", movieIdString), Arrays.asList("rating"));
-            Value nameValue = Value.createValue(resultSet.getString(2).getBytes());
-            Value yearValue = Value.createValue(resultSet.getString(3).getBytes());
+            Value nameValue = Value.createValue(movieResult.getString(2).getBytes());
+            Value yearValue = Value.createValue(movieResult.getString(3).getBytes());
 
             // Some ranks are null
-            if (resultSet.getString(4) != null) {
-                Value ratingValue = Value.createValue(resultSet.getString(4).getBytes());
+            if (movieResult.getString(4) != null) {
+                Value ratingValue = Value.createValue(movieResult.getString(4).getBytes());
+                store.put(rating, ratingValue);
+            }
+            store.put(name, nameValue);
+            store.put(year, yearValue);
+        }
+
+        ResultSet roleResult = jdbcStatement.executeQuery("SELECT id, name, year, rank FROM Movie");
+
+        // Get the Role table for the GetMovieActorRoles
+        while (roleResult.next()) {
+            System.out.println(roleResult.getInt(1) + "\t" + roleResult.getString(2) + "\t"
+                    + roleResult.getString(3) + "\t" + roleResult.getString(4));
+
+            String movieIdString = roleResult.getString(1);
+            Key actorId = Key.createKey(Arrays.asList("movie", movieIdString), Arrays.asList("name"));
+            Key movieId = Key.createKey(Arrays.asList("movie", movieIdString), Arrays.asList("year"));
+            Key role = Key.createKey(Arrays.asList("movie", movieIdString), Arrays.asList("rating"));
+            Value nameValue = Value.createValue(movieResult.getString(2).getBytes());
+            Value yearValue = Value.createValue(movieResult.getString(3).getBytes());
+
+            // Some ranks are null
+            if (movieResult.getString(4) != null) {
+                Value ratingValue = Value.createValue(movieResult.getString(4).getBytes());
                 store.put(rating, ratingValue);
             }
             store.put(name, nameValue);
@@ -41,7 +64,7 @@ public class LoadDB {
 
 
 
-        resultSet.close();
+        movieResult.close();
         jdbcStatement.close();
         jdbcConnection.close();
     }
